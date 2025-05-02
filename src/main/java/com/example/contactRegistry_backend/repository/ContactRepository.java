cb715f2ae71f8ddf061e9f4a6bf5fb5898d9e5d7
Morgan Ogola <morganogola@gmail.com>
2025-05-02 15:14:52 +0300

    add jpa finder methods to find contacts by phone number, email, and ID number

--- src/main/java/com/example/contactRegistry_backend/repository/ContactRepository.java
+++ src/main/java/com/example/contactRegistry_backend/repository/ContactRepository.java
@@ -1,7 +1,15 @@
 package com.example.contactRegistry_backend.repository;
 
+import com.example.contactRegistry_backend.entity.Contact;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ContactRepository {
+    Optional<Contact> findByPhoneNumber(String phoneNumber);
+
+    Optional<Contact> findByEmail(String email);
+
+    Optional<Contact> findByIdNumber(String idNumber);
 }
